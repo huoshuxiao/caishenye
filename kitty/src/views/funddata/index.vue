93191f8ade0c69d16a886f12ad460ff1ad817486
TD <13555817@qq.com>
2020-03-07 22:58:22 +0800

    添加mongo

---
+++ kitty/src/views/funddata/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home-container">
+    <span>1123</span>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+<style lang="scss" scoped>
+.home-container {
+
+}
+</style>
