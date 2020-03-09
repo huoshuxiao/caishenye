fee15a404ab1571499cd2ba3f20a8277dbf5ffa3
sunwenkun <sunwenkun@gmail.com>
2020-03-09 23:08:04 +0800

    init project

---
+++ rubikcube/src/main/java/com/sun/caishenye/rubikcube/RubikcubeApplication.java
@@ -0,0 +1,13 @@
+package com.sun.caishenye.rubikcube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubikcubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RubikcubeApplication.class, args);
+    }
+
+}
---
+++ rubikcube/src/test/java/com/sun/caishenye/rubikcube/RubikcubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun.caishenye.rubikcube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubikcubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
