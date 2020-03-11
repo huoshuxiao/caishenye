ca1996e8bafea69ace6c1ceb7bc478d7b85cce9e
huoshuxiao <huoshuxiao@gmail.com>
2020-03-11 14:39:47 +0800

    注释掉mongo调用

--- octopus/src/main/java/com/sun/caishenye/octopus/fund/domain/FundDataMongoDomain.java
+++ octopus/src/main/java/com/sun/caishenye/octopus/fund/domain/FundDataMongoDomain.java
@@ -1,33 +1,33 @@
-package com.sun.caishenye.octopus.fund.domain;
-
-import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.io.Serializable;
-
-/**
- * @author Administrator
- */
-@Data
-@Document(collection = "t_fund_data1")
-public class FundDataMongoDomain implements Serializable {
-    private String fund_code;
-    private String fund_name;
-    private String net_value_date;
-    private String unit_net;
-    private String risk;
-    private String set_up_date;
-    private String avg_year_profit;
-    private String day_profit;
-    private String week_profit;
-    private String month_profit;
-    private String quarter_profit;
-    private String half_a_year;
-    private String year_profit;
-    private String two_year_profit;
-    private String three_year_profit;
-    private String five_year_profit;
-    private String ten_year_profit;
-    private String total_profit;
-}
+//package com.sun.caishenye.octopus.fund.domain;
+//
+//import lombok.Data;
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.mongodb.core.mapping.Document;
+//
+//import java.io.Serializable;
+//
+///**
+// * @author Administrator
+// */
+//@Data
+//@Document(collection = "t_fund_data1")
+//public class FundDataMongoDomain implements Serializable {
+//    private String fund_code;
+//    private String fund_name;
+//    private String net_value_date;
+//    private String unit_net;
+//    private String risk;
+//    private String set_up_date;
+//    private String avg_year_profit;
+//    private String day_profit;
+//    private String week_profit;
+//    private String month_profit;
+//    private String quarter_profit;
+//    private String half_a_year;
+//    private String year_profit;
+//    private String two_year_profit;
+//    private String three_year_profit;
+//    private String five_year_profit;
+//    private String ten_year_profit;
+//    private String total_profit;
+//}
--- octopus/src/test/java/com/sun/caishenye/octopus/MongdbApplicationTests.java
+++ octopus/src/test/java/com/sun/caishenye/octopus/MongdbApplicationTests.java
@@ -1,49 +1,49 @@
-package com.sun.caishenye.octopus;
-
-import com.sun.caishenye.octopus.fund.dao.FundDataMongoDao;
-import com.sun.caishenye.octopus.fund.domain.FundDataMongoDomain;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
-
-@SpringBootTest
-public class MongdbApplicationTests {
-    @Autowired
-    private FundDataMongoDao fundDataMongoDao;
-
-    @Test
-    public void mytest(){
-        this.save();
-        this.findAll();
-    }
-
-
-    public void findAll() {
-        List<FundDataMongoDomain> all = fundDataMongoDao.findAll();
-        System.out.println(all.size());
-    }
-
-
-    public void save() {
-        FundDataMongoDomain fundDataMongoDomain = new FundDataMongoDomain();
-        fundDataMongoDomain.setFund_code("999999");
-        fundDataMongoDomain.setFund_name("金牌基金");
-        fundDataMongoDao.save(fundDataMongoDomain);
-    }
-
-
-    public void update() {
-        FundDataMongoDomain fundDataMongoDomain = new FundDataMongoDomain();
-        fundDataMongoDomain.setFund_code("999999");
-        fundDataMongoDomain.setFund_name("银牌基金");
-        fundDataMongoDao.update(fundDataMongoDomain);
-    }
-
-
-    public void delete() {
-        fundDataMongoDao.delete("61");
-    }
-}
+//package com.sun.caishenye.octopus;
+//
+//import com.sun.caishenye.octopus.fund.dao.FundDataMongoDao;
+//import com.sun.caishenye.octopus.fund.domain.FundDataMongoDomain;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import java.util.List;
+//
+//@SpringBootTest
+//public class MongdbApplicationTests {
+//    @Autowired
+//    private FundDataMongoDao fundDataMongoDao;
+//
+//    @Test
+//    public void mytest(){
+//        this.save();
+//        this.findAll();
+//    }
+//
+//
+//    public void findAll() {
+//        List<FundDataMongoDomain> all = fundDataMongoDao.findAll();
+//        System.out.println(all.size());
+//    }
+//
+//
+//    public void save() {
+//        FundDataMongoDomain fundDataMongoDomain = new FundDataMongoDomain();
+//        fundDataMongoDomain.setFund_code("999999");
+//        fundDataMongoDomain.setFund_name("金牌基金");
+//        fundDataMongoDao.save(fundDataMongoDomain);
+//    }
+//
+//
+//    public void update() {
+//        FundDataMongoDomain fundDataMongoDomain = new FundDataMongoDomain();
+//        fundDataMongoDomain.setFund_code("999999");
+//        fundDataMongoDomain.setFund_name("银牌基金");
+//        fundDataMongoDao.update(fundDataMongoDomain);
+//    }
+//
+//
+//    public void delete() {
+//        fundDataMongoDao.delete("61");
+//    }
+//}
