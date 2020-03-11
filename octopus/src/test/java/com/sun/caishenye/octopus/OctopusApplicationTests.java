819b992451bb267a2751203e45841d2c9b466bbb
huoshuxiao <huoshuxiao@gmail.com>
2020-03-11 21:16:38 +0800

    添加注释

--- octopus/src/test/java/com/sun/caishenye/octopus/OctopusApplicationTests.java
+++ octopus/src/test/java/com/sun/caishenye/octopus/OctopusApplicationTests.java
@@ -2,8 +2,13 @@ package com.sun.caishenye.octopus;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+// If your test is @Transactional, it rolls back the transaction at the end of each test method by default.
+// However, as using this arrangement with either RANDOM_PORT or DEFINED_PORT implicitly provides a real servlet environment
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+// If you are using JUnit 4, don’t forget to also add @RunWith(SpringRunner.class) to your test
+//@RunWith(SpringRunner.class)
 class OctopusApplicationTests {
 
     @Test
