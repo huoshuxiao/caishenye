7aaae53c8d803b8ca287f61f53770d9251b20171
huoshuxiao <huoshuxiao@gmail.com>
2020-03-17 11:53:24 +0800

    init module 太极 (Tai chi) 服务中心

---
+++ taichi/src/main/java/com/sun/caishenye/taichi/TaichiApplication.java
@@ -0,0 +1,13 @@
+package com.sun.caishenye.taichi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaichiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaichiApplication.class, args);
+    }
+
+}
