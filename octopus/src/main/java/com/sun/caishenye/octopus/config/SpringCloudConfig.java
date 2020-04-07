2cf660b9abc96f9321391b423813c954b0ce68ae
huoshuxiao <huoshuxiao@gmail.com>
2020-04-07 22:00:34 +0800

    config server

---
+++ octopus/src/main/java/com/sun/caishenye/octopus/config/SpringCloudConfig.java
@@ -0,0 +1,10 @@
+package com.sun.caishenye.octopus.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * SpringCould 配置类
+ */
+@Configuration
+public class SpringCloudConfig {
+}
