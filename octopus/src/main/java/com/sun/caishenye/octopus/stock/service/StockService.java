1ef90a307e1c34dc417a6b1ed0485d6407a4dc2a
huoshuxiao <huoshuxiao@gmail.com>
2020-04-20 22:42:25 +0800

    bug 历史行情 进度错误数据处理

--- octopus/src/main/java/com/sun/caishenye/octopus/stock/service/StockService.java
+++ octopus/src/main/java/com/sun/caishenye/octopus/stock/service/StockService.java
@@ -16,6 +16,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -263,6 +265,7 @@ public class StockService {
 
             log.debug("mm stockDomain value>> {}", stockDomain);
 
+            // 预案
             // 当年 股息率 预测,用 实时行情 数据 计算
             ShareBonusDomain sbDomain = stockDomain.getSbDomain();
             log.debug("mm sbDomain value>> {}", sbDomain);
@@ -280,24 +283,53 @@ public class StockService {
                     // 股息率 = 派息(税前)(元) / 股价
                     stockDomain.setDividendYield(calDividendYield(stockDomain));
                 }
+            // 实施
             // 历史 股息率,用 历史行情 数据 计算
             } else if (sbDomain.getSchedule().equals(Constants.SB_SCHEDULE_IMPLEMENT.getString())
                     && Double.valueOf(sbDomain.getDividend()).doubleValue() > 0d) { // 分红金额大于0
+
                 // 根据 公司代码 + 除权除息日(map) 取得 股价
                 String mapKey = stockDomain.getCompanyCode() + Utils.formatDate2String(sbDomain.getDividendDate());
+                log.debug("mm mapKey :: {}", mapKey);
 //                DayLineDomain dayLineDomain = hhqDataMap.get(mapKey);
                 DayLineDomain dayLineDomain = apiRestTemplate.getHhqByDateForObject(stockDomain);
                 // 历史数据 没有时 不计算股息率
                 if (dayLineDomain == null) {
                     log.warn("mm mapKey :: {}", mapKey);
-                    stockDomain.setDividendYield("×");
-                    stockDomain.setPrice("×");
+                    if ("--".equals(sbDomain.getDividendDate())) {
+                        stockDomain.setDividendYield("×");
+                        stockDomain.setPrice("×");
+                        stockDomain.setDate(sbDomain.getDividendDate());
+                    } else {
+                        // 除权除息日 大于等于 当日（数据问题） 取实时行情
+                        LocalDate today = LocalDate.now();
+                        LocalDate dividendLocalDate = LocalDate.of(Integer.valueOf(sbDomain.getDividendDate().substring(0, 4)).intValue(),
+                                Integer.valueOf(sbDomain.getDividendDate().substring(5, 7)).intValue(),
+                                Integer.valueOf(sbDomain.getDividendDate().substring(8, 10)).intValue());
+                        if (ChronoUnit.DAYS.between(dividendLocalDate, today) <= 0) {
+                            // 实时行情 股价
+                            String price = hqDataMap.get(stockDomain.getCompanyCode()).getPrice();
+                            stockDomain.setPrice(price);
+                            stockDomain.setDate(hqDataMap.get(stockDomain.getCompanyCode()).getDate());
+                            // 停牌时 不计算股息率
+                            if (Constants.HQ_SUSPENSION.getString().equals(price)) {
+                                stockDomain.setDividendYield("-");
+                            } else {
+                                // 股息率 = 派息(税前)(元) / 股价
+                                stockDomain.setDividendYield(calDividendYield(stockDomain));
+                            }
+                        } else {
+                            stockDomain.setDividendYield("×");
+                            stockDomain.setPrice("×");
+                            stockDomain.setDate(sbDomain.getDividendDate());
+                        }
+                    }
                 } else {
                     stockDomain.setPrice(dayLineDomain.getPrice());
+                    stockDomain.setDate(sbDomain.getDividendDate());
                     // 股息率 = 派息(税前)(元) / 股价
                     stockDomain.setDividendYield(calDividendYield(stockDomain));
                 }
-                stockDomain.setDate(sbDomain.getDividendDate());
             // 不分配 股息率=0
             } else if (sbDomain.getSchedule().equals(Constants.SB_SCHEDULE_NOT_ASSIGNED.getString())) {
 //                // 不分配 无除权除息日,取公告日作为除权除息日
