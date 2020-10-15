8faa909ed7740955b82225aad4cdae602ed5872c
huoshuxiao <huoshuxiao@gmail.com>
2020-10-16 07:29:51 +0800

    retry

--- octopus/src/main/java/com/sun/caishenye/octopus/job/AgentJob.java
+++ octopus/src/main/java/com/sun/caishenye/octopus/job/AgentJob.java
@@ -4,13 +4,13 @@ import com.sun.caishenye.octopus.fund.service.FundService;
 import com.sun.caishenye.octopus.stock.service.StockService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
@@ -31,9 +31,18 @@ public class AgentJob {
     }
 
     @Scheduled(cron = "${job.cron.stock}")
-    public void stock() throws ExecutionException, InterruptedException {
+    public void stock() throws InterruptedException {
         LocalDateTime startTime = LocalDateTime.now();
-        stockService.run();
+        try {
+            stockService.run();
+        } catch (ExecutionException e) {
+            try {
+                TimeUnit.MINUTES.sleep(30);
+                stockService.run();
+            } catch (ExecutionException executionException) {
+                log.error("stock job run error :: {}", e);
+            }
+        }
         LocalDateTime endTime = LocalDateTime.now();
         log.info("stock job run :: {} minutes", ChronoUnit.MINUTES.between(startTime, endTime));
     }
