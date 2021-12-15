65ddcebe275049a0a8c19f331a90a30971dc1e2f
huoshuxiao <huoshuxiao@gmail.com>
2021-12-15 20:31:51 +0800

    delete kitty files

--- kitty/settings.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  projectName: 'kitty',
-  port: 8080,
-  host: '0.0.0.0'
-}
--- kitty/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import { autoLoadModule } from './util'
-
-Vue.use(Vuex)
-
-export default new Vuex.Store({
-  modules: autoLoadModule()
-})
