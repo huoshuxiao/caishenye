d2fde0922328645fa75095dd827624f487f3eeb6
huoshuxiao <huoshuxiao@gmail.com>
2024-04-02 21:06:23 +0800

    cube
    add comment

--- cube/src/main/python/com/sun/caishenye/cube/stock/money_more.py
+++ cube/src/main/python/com/sun/caishenye/cube/stock/money_more.py
@@ -5,18 +5,24 @@ file_path = config.get('file.path')
 file_name = config.get('file.name.stock.mm')
 headers = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']
 header_h = ['预案']
-header_g = '4'
+header_g = config.get('file.name.stock.header.g')
 
 
 def y_current():
     df = __read_file()
     log.log(df)
 
+    """
+    过滤数据。
+    """
     # df_filtered = df[df['E'].str.contains(utils.year()) & df['H'].isin(header_h) & df['G'] > 5]
     df_filtered1 = df[df['E'].str.contains(utils.year()) & df['H'].isin(header_h)]
     df_filtered2 = df_filtered1[df_filtered1['G'] >= header_g]
     log.log(df_filtered2)
 
+    """
+    写excel。
+    """
     df_sort = df_filtered2.sort_values(by='G', ascending=False)
     log.log(df_sort)
     utils.write_excel(file_path, 'mm', df_sort)
