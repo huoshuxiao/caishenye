5aebcd64d109a8031bfebb046f7b4db943f9d484
huoshuxiao <huoshuxiao@gmail.com>
2024-04-08 16:13:30 +0800

    Y5TOP5
    done

--- cube/src/main/python/com/sun/caishenye/cube/stock/money_more.py
+++ cube/src/main/python/com/sun/caishenye/cube/stock/money_more.py
@@ -8,14 +8,15 @@ file_name = config.get('file.name.stock.mm.file_name')
 headers = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']
 year_target = config.get('file.name.stock.mm.target_year')
 out_file_name = config.get('file.name.stock.mm.out_file_name')
+header_g = config.get('file.name.stock.mm.header.g')
 
 
 def run():
     utils.remove_file(file_path, out_file_name)
 
     __y_current__()
-    __y5_top5__()
     __y2_top6__()
+    __y5_top5__()
 
 
 def __y_current__():
@@ -25,8 +26,6 @@ def __y_current__():
     过滤数据。
     """
     header_h = ['预案']
-    header_g = config.get('file.name.stock.mm.header.g')
-    # df_filtered = df[df['E'].str.contains(utils.year()) & df['H'].isin(header_h) & df['G'] > 5]
     df_filtered1 = df[df['E'].str.contains(r'{}'.format(utils.year())) & df['H'].isin(header_h)]
     df_filtered1['G'] = df_filtered1['G'].astype(float)
     df_filtered2 = df_filtered1[df_filtered1['G'] >= header_g]
@@ -34,13 +33,134 @@ def __y_current__():
     """
     写excel。
     """
-    df_sort = df_filtered2.sort_values(by='G', ascending=False)
-    log.log(df_sort)
-    utils.write_excel(file_path, out_file_name, 'mm', df_sort)
+    df_result = df_filtered2.sort_values(by='G', ascending=False)
+    utils.write_excel(file_path, out_file_name, 'mm', df_result)
+    log.log(r'MM :: {}'.format(df_result))
 
 
 def __y5_top5__():
     df = __read_file__()
+
+    """
+    过滤数据。(近六年)
+    """
+    header_h = ['实施']
+    # TODO
+    year_current = r'{}'.format(year_target + 1)
+    year_current_1 = r'{}'.format(year_target)
+    year_current_2 = r'{}'.format(year_target - 1)
+    year_current_3 = r'{}'.format(year_target - 2)
+    year_current_4 = r'{}'.format(year_target - 3)
+    year_current_5 = r'{}'.format(year_target - 4)
+    years = [year_current, year_current_1, year_current_2, year_current_3, year_current_4, year_current_5]
+    df_filtered1 = df[df['E'].str.contains('|'.join(years)) & df['H'].isin(header_h)]  # dataframe
+    df_filtered1['G'] = df_filtered1['G'].astype(float)
+    df2 = df_filtered1[df_filtered1['G'] >= header_g]  # series
+    # utils.write_excel(file_path, out_file_name, '1_Y5TOP5_6Y', df2.sort_values(by='A'))
+
+    """
+    计算
+    """
+
+    """
+    1. 评分。
+    2. 均值(mean)。
+    """
+    # 1. 评分
+    df2['L'] = ''
+    df2['M'] = df2['B'].map(df2['B'].value_counts())
+    utils.write_excel(file_path, out_file_name, '2_Y5TOP5_COUNT', df2.sort_values(by='A'))
+
+    # TODO
+    year_current = r'{}'.format(year_target)
+    year_current_1 = r'{}'.format(year_target - 1)
+    year_current_2 = r'{}'.format(year_target - 2)
+    year_current_3 = r'{}'.format(year_target - 3)
+    year_current_4 = r'{}'.format(year_target - 4)
+    year_current_5 = r'{}'.format(year_target - 5)
+    years = [year_current, year_current_1, year_current_2, year_current_3, year_current_4, year_current_5]
+    # 计算第一名
+    df_score_1 = __cal_y5_top5_score__(df2, years, 6)
+
+    # 计算第二名
+    df_score_2_5 = __cal_y5_top5_score__(df2, years[:5], 5)
+    df_score_2_3 = __cal2_y5_top5_score__(df_score_2_5, years[:4], '5-')
+    df_score_2 = pd.concat([df_score_2_5[df_score_2_5['M'].eq(5)], df_score_2_3])
+    df_score_2_other = pd.concat([df_score_2_5, df_score_2]).drop_duplicates(keep=False)
+    df_score_2_other['M'] = '3-'
+
+    # 计算第三名
+    df_score_3_5 = __cal_y5_top5_score__(df2, years[:4], 4)
+    df_score_3_3 = __cal2_y5_top5_score__(df_score_3_5, years[:3], '4-')
+    df_score_3 = pd.concat([df_score_3_5[df_score_3_5['M'].eq(4)], df_score_3_3])
+    df_score_3_other = pd.concat([df_score_3_5, df_score_3]).drop_duplicates(keep=False)
+    df_score_3_other['M'] = '3--'
+
+    # 计算第四名
+    df_score_4 = __cal_y5_top5_score__(df2, years[:3], 3)
+    # 过滤3-
+    df_score_4 = df_score_4[~df_score_4['M'].eq('3-')]
+
+    # 评分3以上
+    df_score = pd.concat([df_score_1, df_score_2, df_score_3, df_score_4, df_score_2_other, df_score_3_other])
+    utils.write_excel(file_path, out_file_name, '2_Y5TOP5_RECOUNT', df_score)
+
+    # 2. 均值(mean)[以B列分组计算G列的均值(mean)]
+    df_score_1['N'] = df_score_1.groupby('B')['G'].transform('mean')
+    df_score_2['N'] = df_score_2.groupby('B')['G'].transform('mean')
+    df_score_3['N'] = df_score_3.groupby('B')['G'].transform('mean')
+    df_score_4['N'] = df_score_4.groupby('B')['G'].transform('mean')
+    df_score_2_other['N'] = df_score_2_other.groupby('B')['G'].transform('mean')
+    df_score_3_other['N'] = df_score_3_other.groupby('B')['G'].transform('mean')
+    df_score = pd.concat([df_score_1, df_score_2, df_score_3, df_score_4, df_score_2_other, df_score_3_other])
+    utils.write_excel(file_path, out_file_name, '3_Y5TOP5_MEAN', df_score)
+
+    # 为了不影响列的顺序，以A列分组(不写reset_index B列缺失)
+    df_score_1 = df_score_1.groupby('A').agg({col: 'first' for col in df_score_1.columns if col != 'A'}).reset_index()
+    df_score_2 = df_score_2.groupby('A').agg({col: 'first' for col in df_score_2.columns if col != 'A'}).reset_index()
+    df_score_3 = df_score_3.groupby('A').agg({col: 'first' for col in df_score_3.columns if col != 'A'}).reset_index()
+    df_score_4 = df_score_4.groupby('A').agg({col: 'first' for col in df_score_4.columns if col != 'A'}).reset_index()
+    df_score_2_other = df_score_2_other.groupby('A').agg({col: 'first' for col in df_score_2_other.columns if col != 'A'}).reset_index()
+    df_score_3_other = df_score_3_other.groupby('A').agg({col: 'first' for col in df_score_3_other.columns if col != 'A'}).reset_index()
+
+    df_score_1 = df_score_1.sort_values(by=['M', 'N'], ascending=False)
+    df_score_2 = df_score_2.sort_values(by=['M', 'N'], ascending=False)
+    df_score_3 = df_score_3.sort_values(by=['M', 'N'], ascending=False)
+    df_score_4 = df_score_4.sort_values(by=['M', 'N'], ascending=False)
+    df_score_2_other = df_score_2_other.sort_values(by=['M', 'N'], ascending=False)
+    df_score_3_other = df_score_3_other.sort_values(by=['M', 'N'], ascending=False)
+
+    df_score = pd.concat([df_score_1, df_score_2, df_score_3, df_score_4, df_score_2_other, df_score_3_other])
+    utils.write_excel(file_path, out_file_name, 'Y5TOP5', df_score)
+    log.log(r'Y5TOP5 :: {}'.format(df_score))
+
+
+def __cal_y5_top5_score__(df, years, score):
+    # 1. 评分为6的，且K列包含连续近6年的是第1名
+    df_score_ALL = df[df['M'].eq(score)]
+    df_score = df[df['M'].eq(score) & df['K'].str.contains('|'.join(years))]
+    df_score['M'] = df_score['B'].map(df_score['B'].value_counts())
+    # 1. 评分为6的，且K列包含连续近6年的是第1名
+    df_score_A = df_score[df_score['M'].eq(score)]
+    # 1. 评分为6的，且K列不是连续近6年的是第1名-
+    df_score_B = pd.concat([df_score_ALL, df_score_A]).drop_duplicates(keep=False)
+    df_score_B['M'] = r'{}-'.format(score)
+    df_score = pd.concat([df_score_A, df_score_B])
+
+    return df_score
+
+
+def __cal2_y5_top5_score__(df, years, score):
+    # 1. 评分为6的，且K列包含连续近6年的是第1名
+    df_score_ALL = df[df['M'].eq(score)]
+    df_score = df[df['M'].eq(score) & df['K'].str.contains('|'.join(years))]
+    df_score['M'] = df_score['B'].map(df_score['B'].value_counts())
+    # 1. 评分为6的，且K列包含连续近6年的是第1名
+    df_score = df_score[df_score['M'].eq(len(years))]
+    df_score = df_score_ALL[df_score_ALL['B'].isin(df_score['B'])]
+    df_score['M'] = score
+
+    return df_score
 
 
 def __y2_top6__():
@@ -50,7 +170,6 @@ def __y2_top6__():
     过滤数据。(近三年)
     """
     header_h = ['实施']
-    header_g = 6.0
     # TODO
     year_current = r'{}'.format(year_target + 1)
     year_current_1 = r'{}'.format(year_target)
@@ -58,8 +177,8 @@ def __y2_top6__():
     years = [year_current, year_current_1, year_current_2]
     df_filtered1 = df[df['E'].str.contains('|'.join(years)) & df['H'].isin(header_h)]  # dataframe
     df_filtered1['G'] = df_filtered1['G'].astype(float)
-    df2 = df_filtered1[df_filtered1['G'] >= header_g]  # series
-    utils.write_excel(file_path, out_file_name, '1_Y2TOP6_3Y', df2.sort_values(by='A'))
+    df2 = df_filtered1[df_filtered1['G'] >= header_g + 1]  # series
+    # utils.write_excel(file_path, out_file_name, '1_Y2TOP6_3Y', df2.sort_values(by='A'))
 
     """
     计算
@@ -68,27 +187,31 @@ def __y2_top6__():
     1. 评分。
         1. 评分为3的，第一名
         2. 评分为2的，且K列包含连续近两年的是第二名
-        3. M列，G列平均值排序
+        3. M列，G列平均值排序(以B列分组计算G列的均值(mean))
     """
     df2['L'] = ''
     df2['M'] = df2['B'].map(df2['B'].value_counts())
     utils.write_excel(file_path, out_file_name, '2_Y2TOP6_COUNT', df2.sort_values(by='A'))
 
+    # 1. 评分为3的，第一名
     df_score_1 = df2[df2['M'].eq(3)]
 
+    # TODO
+    # 2. 评分为2的，且K列包含连续近两年的是第二名
     year_current = r'{}'.format(year_target)
     year_current_1 = r'{}'.format(year_target - 1)
     years = [year_current, year_current_1]
     df_score_2 = df2[df2['M'].eq(2) & df2['K'].str.contains('|'.join(years))]
     df_score_2['M'] = df_score_2['B'].map(df_score_2['B'].value_counts())
+    # 2. 评分为2的，且K列包含连续近两年的是第二名
     df_score_2 = df_score_2[df_score_2['M'].eq(2)]
 
-    # 以B列分组计算G列的均值(mean)
+    # 3. 以B列分组计算G列的均值(mean)
     df_score_1['N'] = df_score_1.groupby('B')['G'].transform('mean')
     df_score_2['N'] = df_score_2.groupby('B')['G'].transform('mean')
     utils.write_excel(file_path, out_file_name, '3_Y2TOP6_MEAN', pd.concat([df_score_1, df_score_2]).sort_values(by='M'))
 
-    # 为了不影响列的顺序，以A列分组。不写reset_index B列缺失。
+    # 为了不影响列的顺序，以A列分组(不写reset_index B列缺失)
     df_score_1 = df_score_1.groupby('A').agg({col: 'first' for col in df_score_1.columns if col != 'A'}).reset_index()
     df_score_2 = df_score_2.groupby('A').agg({col: 'first' for col in df_score_2.columns if col != 'A'}).reset_index()
 
@@ -96,10 +219,9 @@ def __y2_top6__():
     df_score_2 = df_score_2.sort_values(by='N', ascending=False)
 
     df_score = pd.concat([df_score_1, df_score_2])
-    log.log(df_score)
     utils.write_excel(file_path, out_file_name, 'Y2TOP6', df_score)
+    log.log(r'Y2TOP6 :: {}'.format(df_score))
 
 
 def __read_file__():
     return utils.read_csv(file_path, file_name, headers)
-
